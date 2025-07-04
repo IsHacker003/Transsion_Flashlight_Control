ef158414b44f9ecac4a82e85371179bb4759308c
IsHacker <ishayu301@gmail.com>
2025-07-04 16:13:32 +0530

    Permissive SELinux is optional now

--- app/src/main/java/com/ishacker/tranflashcontrol/MainActivity.kt
+++ app/src/main/java/com/ishacker/tranflashcontrol/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import java.io.FileOutputStream
@@ -16,61 +17,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.button)
     }
-    @SuppressLint("ServiceCast")
     private fun openFlashLight() {
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         val cameraId = cameraManager.cameraIdList[0]
         cameraManager.setTorchMode(cameraId, true)
     }
-    private fun openSubFlashLight() {
+    fun openSubFlashLight(view: View) {
         val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         val cameraId = cameraManager.cameraIdList[1]
         cameraManager.setTorchMode(cameraId, true)
     }
-
-    fun TurnFlashlightOn() {
-        val iIntValue = 1
-        val fileOutputStream = FileOutputStream("/sys/class/torch/torch/torch_level")
-        fileOutputStream.write(iIntValue.toString().toByteArray())
-        fileOutputStream.close()
-        openFlashLight()
+    fun closeFlashLight(view: View) {
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[0]
+        cameraManager.setTorchMode(cameraId, false)
     }
-
-    @Throws(IOException::class)
-    fun TurnFlashlightOff(p0: View?) {
-        val iIntValue = 0
-        val fileOutputStream = FileOutputStream("/sys/class/torch/torch/torch_level")
-        fileOutputStream.write(iIntValue.toString().toByteArray())
-        fileOutputStream.close()
+    fun closeSubFlashLight(view: View) {
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        val cameraId = cameraManager.cameraIdList[1]
+        cameraManager.setTorchMode(cameraId, false)
     }
-
-    @Throws(IOException::class)
-    fun TurnSubFlashlightOn(p0: View?) {
-        val iIntValue = 1
-        val fileOutputStream =
-            FileOutputStream("/sys/devices/virtual/sub_torch/sub_torch/sub_torch_level")
-        fileOutputStream.write(iIntValue.toString().toByteArray())
-        fileOutputStream.close()
-        openSubFlashLight()
-    }
-
-    @Throws(IOException::class)
-    fun TurnSubFlashlightOff(p0: View?) {
-        val iIntValue = 0
-        val fileOutputStream =
-            FileOutputStream("/sys/devices/virtual/sub_torch/sub_torch/sub_torch_level")
-        fileOutputStream.write(iIntValue.toString().toByteArray())
-        fileOutputStream.close()
-    }
-
     fun setPermissive(p0: View?) {
         Toast.makeText(this, "Please grant root", Toast.LENGTH_LONG).show();
         Runtime.getRuntime().exec("/debug_ramdisk/su -c /system/bin/setenforce 0")
     }
 
     override fun onClick(p0: View?) {
-        TurnFlashlightOn()
+        openFlashLight()
     }
-
-
 }
